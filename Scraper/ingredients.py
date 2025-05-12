3fb1a28f8bf05c5d29c0e05fc398b36f47a88a85
Bunnypop <133989817+Bunnypop24@users.noreply.github.com>
2025-05-12 21:04:32 +0200

    Scraper for base ingreadients from api done

--- Scraper/ingredients.py
+++ Scraper/ingredients.py
@@ -4,14 +4,22 @@ import psycopg2
 from psycopg2 import sql
 import datetime
 import string
+import os
+from dotenv import load_dotenv
 
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 
+# Connect to database
 conn = psycopg2.connect(
-    dbname="Test-1",
-    user="postgres",
-    password="Glufgluf",
-    host="127.0.0.1",
-    port="5432"
+    dbname=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    port=DB_PORT
 ) #Connecting to the database
 
 cur = conn.cursor()
